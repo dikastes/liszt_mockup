7ddef2f6bb207512d087be1142e883d088d88ab7
thomas-sc <info@schefter.net>
2024-02-13 13:15:45 +0100

    Add text-collapsing feature for long descriptions and enhance navigation interactivity

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,24 @@ document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new Popover(popover)
   })
+
+// first simple variant for the text-collapse of long texts after 12 Lines
+// Todo: animation?, labels in language and number of lines from data attribute?
+    document.querySelectorAll('.text-collapse').forEach(function(el) {
+        let lineHeigtInPx = parseFloat(getComputedStyle(el).lineHeight);
+        if(el.scrollHeight > lineHeigtInPx * 12){
+            let button = document.createElement('button');
+            button.innerText = 'Gesamten Text anzeigen';
+            button.className = 'show-more btn btn-link ps-0';
+            button.setAttribute('aria-expanded', "false");
+            el.parentNode.insertBefore(button, el.nextSibling);
+            button.style.display = "block";
+            button.addEventListener('click', function(){
+                el.style.webkitLineClamp = 'unset';
+                el.style.overflow = 'visible';
+                button.setAttribute('aria-expanded', "true");
+                button.style.display = "none";
+            });
+        }
+    });
+
