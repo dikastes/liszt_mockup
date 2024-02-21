aa6bf6c735e69afa5d720fcc48b4220194de2e3f
thomas-sc <info@schefter.net>
2024-02-21 15:18:29 +0100

    change vite.conf for multiple html input files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { resolve } from 'path'
 import handlebars from 'vite-plugin-handlebars';
+import path from 'path';
+import fs from 'fs';
 
+function getHtmlEntries() {
+    const pagesDir = path.resolve(__dirname, "src");
+    const entries = {};
+
+    // Read all files in the directory
+    const files = fs.readdirSync(pagesDir);
+
+    // Filter out HTML files
+    const htmlFiles = files.filter((file) => file.endsWith(".html"));
+
+    // Create entries for each HTML file
+    htmlFiles.forEach((file) => {
+        const name = path.basename(file, ".html");
+        entries[name] = path.resolve(pagesDir, file);
+    });
+
+    return entries;
+}
 export default {
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+      rollupOptions: {
+          input: getHtmlEntries(),
+        }
   },
     base: '/liszt_mockup/',
   server: {
